431916ca330761299fc11f77f3280d5f52cf34bc
Beatriz043545 <153848864+Beatriz043545@users.noreply.github.com>
2024-06-17 13:13:56 +0100

    Add files via upload

---
+++ SRC/controllers/src/_tests_/acceptance/test-helper.ts
@@ -0,0 +1,32 @@
+import {ControllersApplication} from '../..';
+import {
+  createRestAppClient,
+  givenHttpServerConfig,
+  Client,
+} from '@loopback/testlab';
+
+export async function setupApplication(): Promise<AppWithClient> {
+  const restConfig = givenHttpServerConfig({
+    // Customize the server configuration here.
+    // Empty values (undefined, '') will be ignored by the helper.
+    //
+    // host: process.env.HOST,
+    // port: +process.env.PORT,
+  });
+
+  const app = new ControllersApplication({
+    rest: restConfig,
+  });
+
+  await app.boot();
+  await app.start();
+
+  const client = createRestAppClient(app);
+
+  return {app, client};
+}
+
+export interface AppWithClient {
+  app: ControllersApplication;
+  client: Client;
+}
