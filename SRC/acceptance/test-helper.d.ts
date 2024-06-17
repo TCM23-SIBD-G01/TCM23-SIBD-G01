fb2c32125e3d19603ad31c7eace50dd4d3dd08bb
Beatriz043545 <153848864+Beatriz043545@users.noreply.github.com>
2024-06-17 12:57:19 +0100

    Delete SRC/acceptance directory

--- SRC/acceptance/test-helper.d.ts
+++
@@ -1,7 +0,0 @@
-import { ControllersApplication } from '../..';
-import { Client } from '@loopback/testlab';
-export declare function setupApplication(): Promise<AppWithClient>;
-export interface AppWithClient {
-    app: ControllersApplication;
-    client: Client;
-}
